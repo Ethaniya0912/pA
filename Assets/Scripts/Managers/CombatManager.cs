22bc87b4393b8622a3782ca0b2c7764203ef718b
NAMKYU <rnk505@gmail.com>
2025-07-02 23:23:52 +0900

    v1.14 CombatManager 및 StatsManager 기초 작업, Input Handler 연계는 실패함 Scene 에 Player((GameObject) 추가하고 스크립트 넣어서 테스트 진행함

--- Assets/Scripts/Managers/CombatManager.cs
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Attack()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        statsManager.TakeDamage(1.0f);
+        Debug.Log("Enemy : Attack!");
     }
 }
--- Assets/Scripts/Managers/StatsManager.cs
+++ Assets/Scripts/Managers/StatsManager.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class StatsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float maxHp = 10f;
+    public float currentHealth = 0;
     void Start()
     {
-        
+        currentHealth = maxHp;
+    }
+    public void TakeDamage(float damage)
+    {
+        Debug.Log(currentHealth);
+        currentHealth -= damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHp);
+        Debug.Log(currentHealth);
+    }
+    public void UpdatePlayerStats()
+    {
+        //점프나 대시할때 스테미나 감소
+    }
+    public void RegenerateStats()
+    {
+        //시간마다 체력 증가
+    }
+    public void Die()
+    {
+        Debug.Log("Player has died.");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
