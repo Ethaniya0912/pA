992f7a4e5922f588c74fcf856b3be4b2b4a0f61a
NAMKYU <rnk505@gmail.com>
2025-07-04 20:29:39 +0900

    v1.20 InventoryManager 작성 준비, StatsManager에 배고픔 및 아이템 관련 스텟 추가

--- Assets/Scripts/Managers/InventoryManager.cs
+++ Assets/Scripts/Managers/InventoryManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
+    PlayerManager player;
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Managers/StatsManager.cs
+++ Assets/Scripts/Managers/StatsManager.cs
@@ -3,27 +3,44 @@ using System.Collections.Generic;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
+//플레이어관리
 public class StatsManager : MonoBehaviour
 {
     PlayerManager player;
 
-    //스탯관련//
-    public float maxHp = 10f;
-    public float currentHealth = 0f;
-    public float maxStamina = 10f;
-    public float currentStamina = 0f;
-    public float HpGenerationRate = 0.01f; // Health regeneration rate per second
-    public float StaminaGenerationRate = 0.01f; // Stamina regeneration rate per second
+    //최대 스탯//
+    public float maxHealth = 100.0f;
+    public float maxStamina = 10.00f;
+    public float maxFood = 100.0f;
+    //현재 스탯//
+    public float currentHealth = 0.0f;
+    public float currentStamina = 0.0f;
+    public float currentFood = 0.0f;
+    //아이템 관련 현재 스탯//
+    public float currentShield = 0.0f;
+    public float currentArmor = 0.0f;
+    public float currentStrength = 0.0f;
+    public float currentCriticalChance = 0.0f;
+    public float currentAttackSpeed = 0.0f;
+    public float currentSpeed = 0.0f;
+    public float currentMaxHit = 0.0f;
+    //회복 관련//
+    public float HpGenerationRate = 0.1f; // Health regeneration rate per second
+    public float StaminaGenerationRate = 0.1f; // Stamina regeneration rate per second
+    public float FoodDecreaseRate = 0.1f; // Food decrease rate per second
+    //죽음 관련//
     public bool isDead = false;
 
     void Start()
     {
-        currentHealth = maxHp;
+        //초기화
+        currentHealth = maxHealth;
         currentStamina = maxStamina;
-
+        currentFood = maxFood;
         player = GetComponent<PlayerManager>();
     }
 
+    //PlayerManager에서 호출되는 함수//
     public void HandleAllStats()
     {
         UpdatePlayerStats();
@@ -33,11 +50,12 @@ public class StatsManager : MonoBehaviour
             Die();
         }
     }
+    // 공격 받았 때 호출되는 함수//
     public void TakeDamage(float damage)
     {
         Debug.Log(currentHealth);
         currentHealth -= damage;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHp);
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
         Debug.Log(currentHealth);
     }
     public void UpdatePlayerStats()
@@ -54,11 +72,20 @@ public class StatsManager : MonoBehaviour
     }
     public void RegenerateStats()
     {
-        currentHealth += HpGenerationRate * Time.deltaTime;
-        currentHealth = Mathf.Clamp(currentHealth, 0, maxHp);
-        currentStamina += StaminaGenerationRate * Time.deltaTime;
-        currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+        if(currentFood >= 0.0) // 포만감 상태일때 회복
+        {
+            //체력 회복
+            currentHealth += HpGenerationRate * Time.deltaTime;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+            //스테미나 회복
+            currentStamina += StaminaGenerationRate * Time.deltaTime;
+            currentStamina = Mathf.Clamp(currentStamina, 0, maxStamina);
+            //회복에 따른 포만감 감소
+            currentFood -= FoodDecreaseRate * Time.deltaTime;
+            currentFood = Mathf.Clamp(currentFood, 0, maxFood);
+        }
     }
+    // 플레이어가 죽었을 때 호출되는 함수//
     public void Die()
     {
         isDead = true;
