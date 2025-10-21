28e0e1c2874e37c71fc99c81ad5e0a556999a3c3
류남규 <>
2025-10-21 17:58:52 +0900

    v1.37 Adding CharacterEffectsManager, MeleeWeaponDamageCollider for taking damage processing

---
+++ Assets/Scripts/Managers/CharactersEffectManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterEffectsManager : MonoBehaviour
+{
+    // 인스턴스 효과 처리 (데미지 받기, 힐링)
+    
+    // 시간에 걸친 효과 (독, 빌드업 되는 무언가)
+
+    // 스태틱 효과 처리 (갑옷등에서 버프 추가/제거) 
+}
