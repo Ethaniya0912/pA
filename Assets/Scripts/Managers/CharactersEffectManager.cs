68bb200a181b6f7cc6a1ce279a2b1e5ee489eb7e
Ethaniya0912 <ksw5395@gmail.com>
2025-10-22 12:37:52 +0900

    v1.371 quick fix for charactersEffectmanager duplicated file issue

--- Assets/Scripts/Managers/CharactersEffectManager.cs
+++ Assets/Scripts/Managers/CharactersEffectManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class CharacterEffectsManager : MonoBehaviour
 {
-    // 인스턴스 효과 처리 (데미지 받기, 힐링)
-    
-    // 시간에 걸친 효과 (독, 빌드업 되는 무언가)
+    // Process Instant Effects (Take Damage, Heal)
 
-    // 스태틱 효과 처리 (갑옷등에서 버프 추가/제거) 
+    // Process Timed Effects (Poison, Build Ups)
+
+    // Process Static Effects (Adding/Removing buffs from talisman ect)
+
+    public void ProcessInstantEffect()
+    {
+        // Take In An Effect
+        // Process It
+    }
 }
